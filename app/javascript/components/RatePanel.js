72a3e846747b1104b80c1d2b04e6b781b09500fa
e.lukin <e.lukin@simcase.ru>
2017-08-18 11:47:52 +0400

    add bootstrap

--- app/javascript/components/RatePanel.js
+++ app/javascript/components/RatePanel.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 export default class RatePanel extends React.Component {
   render() {
     return <div>
-      <Link to='/admin'>Admin</Link>
-      {this.props.rate}
-      RatePanel
+      <div className="menu">
+        <Link to='/admin'>set custom rate</Link>
+      </div>
     </div>
   }
 }
